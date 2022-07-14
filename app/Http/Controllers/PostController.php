a45b80772b00c6140f2e48a32223172012a68315
akmalluthfi <akmalluthfi19@gmail.com>
2022-07-14 19:09:44 +0700

    learn model, controller, collection

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts', [
+            'title' => 'Blog',
+            'posts' => Post::all()
+        ]);
+    }
+
+    public function show($slug)
+    {
+        return view('post', [
+            'title' => 'Detail Post',
+            'post' => Post::find($slug)
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,53 +29,5 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $posts = [
-        [
-            'title' => 'Judul Post Pertama',
-            'slug' => 'judul-post-pertama',
-            'author' => 'Nando Septian',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, iste illum. Officia inventore ipsam architecto voluptates, iste ex veritatis exercitationem. Earum qui amet iusto delectus aliquam eos, ipsam deleniti quod, unde maxime totam sint natus, tenetur itaque quis dolorum praesentium commodi ipsa. Debitis itaque esse repellendus illum delectus. Ratione autem ea, molestiae saepe delectus quis eius magni nostrum doloremque laboriosam, commodi pariatur numquam quos quod id enim iste culpa, aliquid explicabo. Doloribus vitae natus voluptates nam similique possimus cum soluta!'
-        ],
-        [
-            'title' => 'Judul Post Kedua',
-            'slug' => 'judul-post-kedua',
-            'author' => 'Akmal Luthfi',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolorum ea asperiores. Minus aliquid aliquam dolorum quia labore facere cum a obcaecati autem reiciendis, necessitatibus alias excepturi voluptates, velit error qui vitae tempora explicabo repellendus laboriosam at accusamus voluptatem? Voluptatum porro quod incidunt tenetur adipisci? Ducimus dolore adipisci, quam consequatur repudiandae tempora! Est ipsum dolorem autem aliquam laboriosam atque suscipit, eligendi optio non numquam doloribus hic cumque sequi, commodi ut harum. Harum, iste. Nemo maxime assumenda delectus labore fuga minima illum reprehenderit libero ullam tempore quibusdam incidunt iste ab quia, dolor magni eius maiores obcaecati rem. Id sed aperiam nostrum!'
-        ],
-    ];
-    return view('posts', [
-        'title' => 'Blog',
-        'posts' => $posts
-    ]);
-});
-
-Route::get('/blog/{slug}', function ($slug) {
-    $posts = [
-        [
-            'title' => 'Judul Post Pertama',
-            'slug' => 'judul-post-pertama',
-            'author' => 'Nando Septian',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, iste illum. Officia inventore ipsam architecto voluptates, iste ex veritatis exercitationem. Earum qui amet iusto delectus aliquam eos, ipsam deleniti quod, unde maxime totam sint natus, tenetur itaque quis dolorum praesentium commodi ipsa. Debitis itaque esse repellendus illum delectus. Ratione autem ea, molestiae saepe delectus quis eius magni nostrum doloremque laboriosam, commodi pariatur numquam quos quod id enim iste culpa, aliquid explicabo. Doloribus vitae natus voluptates nam similique possimus cum soluta!'
-        ],
-        [
-            'title' => 'Judul Post Kedua',
-            'slug' => 'judul-post-kedua',
-            'author' => 'Akmal Luthfi',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolorum ea asperiores. Minus aliquid aliquam dolorum quia labore facere cum a obcaecati autem reiciendis, necessitatibus alias excepturi voluptates, velit error qui vitae tempora explicabo repellendus laboriosam at accusamus voluptatem? Voluptatum porro quod incidunt tenetur adipisci? Ducimus dolore adipisci, quam consequatur repudiandae tempora! Est ipsum dolorem autem aliquam laboriosam atque suscipit, eligendi optio non numquam doloribus hic cumque sequi, commodi ut harum. Harum, iste. Nemo maxime assumenda delectus labore fuga minima illum reprehenderit libero ullam tempore quibusdam incidunt iste ab quia, dolor magni eius maiores obcaecati rem. Id sed aperiam nostrum!'
-        ],
-    ];
-
-    $currPost = [];
-
-    foreach ($posts as $post) {
-        if ($post['slug'] === $slug) {
-            $currPost = $post;
-        }
-    }
-
-    return view('post', [
-        'title' => 'Detail Post',
-        'post' => $currPost
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('/blog/{slug}', [PostController::class, 'show']);
